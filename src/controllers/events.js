5c5981ff40b500cb0795cad8e1a7022a420af870
d4ndres <d4ndres@gmail.com>
2024-09-20 08:24:23 -0500

    add events

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,11 +1,24 @@
 const { response } = require('express')
 const Event = require('../models/Event')
 
-const getEvents = ( req, res=response ) => {
-  res.status(200).json({
-    ok: true,
-    msg: 'getEvents'
-  })
+const getEvents = async ( req, res=response ) => {
+
+  try {
+    const events = await Event.find().populate('user', ['name'])
+
+    res.status(200).json({
+      ok: true,
+      msg: 'getEvents',
+      data: events
+    })
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      ok: false,
+      smg: 'Comunicarse con el admin'
+    })
+  }
 } 
 
 
@@ -35,18 +48,90 @@ const createEvent = async ( req, res=response ) => {
 
 
 
-const updateEvent = ( req, res=response ) => {
-  res.status(200).json({
-    ok: true,
-    msg: 'updateEvent'
-  })
+const updateEvent = async ( req, res=response ) => {
+  const eventoId = req.params.id
+  const uid = req.uid
+  
+
+  try {
+    const event = await Event.findById( eventoId )
+
+    if( !event ) {
+      return res.status(404).json({
+        ok: false,
+        smg: 'Evento no existente'
+      })
+    }
+    
+    if( event.user.toString() !== uid ) {
+      return res.status(401).json({
+        ok: false,
+        smg: 'No tiene privilegio de editar este evento'
+      })
+    }
+
+    const nuevoEvento = {
+      ...req.body,
+      user: uid
+    }
+
+    const eventoActualizado = await Event.findByIdAndUpdate( eventoId, nuevoEvento, {new: true} )
+
+    res.status(200).json({
+      ok: true,
+      msg: 'updateEvent',
+      data: eventoActualizado
+    })
+    
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      ok: false,
+      smg: 'Comunicarse con el admin'
+    })
+  }
+
+
 } 
 
-const deleteEvent = ( req, res=response ) => {
-  res.status(200).json({
-    ok: true,
-    msg: 'deleteEvent'
-  })
+const deleteEvent = async ( req, res=response ) => {
+  
+  
+  try {
+    const eventoId = req.params.id
+    const uid = req.uid
+    
+    const event = await Event.findById( eventoId )
+
+    if( !event ) {
+      return res.status(404).json({
+        ok: false,
+        smg: 'Evento no existente'
+      })
+    }
+    
+    if( event.user.toString() !== uid ) {
+      return res.status(401).json({
+        ok: false,
+        smg: 'No tiene privilegio de eliminar este evento'
+      })
+    }
+
+
+
+    await Event.findByIdAndDelete( eventoId )
+
+    res.status(200).json({
+      ok: true
+    })
+    
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      ok: false,
+      smg: 'Comunicarse con el admin'
+    })
+  }
 }
 
 
